b6d2062606b493dbfaa830669942823e36aabf1c
versed-developer <sen.developer@yandex.com>
2024-03-10 10:21:18 +0100

    Implement 5 steps as described

--- evaluate.py
+++ evaluate.py
@@ -3,26 +3,24 @@ import os
 import pandas as pd
 from tensorflow.keras.models import load_model
 
-from lstm import load_data, get_phase_labels, add_or_update_excel_sheet
+from lstm import load_data, write_results
 
 model_path = './lstm_model.keras'
-phase_list = ['A', 'B', 'C']
 
 
 def evaluate_saved_models():
+    test_file_name = input('Please enter excel file name that includes test data:(testing?)').strip() or 'testing'
     if os.path.exists(model_path):
         model = load_model(model_path)
         print("Model loaded successfully.")
 
         # Final evaluation of the model using testing.xlsx
-        x_test, y_test, index = load_data('testing.xlsx')
+        x_test, y_test, index, g_truth = load_data(f'{test_file_name}.xlsx')
         scores = model.evaluate(x_test, y_test, verbose=0)
         print("Accuracy: %.2f%%" % (scores[1] * 100))
 
         predicted_values = model.predict(x_test)
-        phase_labels = get_phase_labels(predicted_values)
-        predicted_df = pd.DataFrame(phase_labels, index=index, columns=['Predicted_PHASE'])
-        add_or_update_excel_sheet(f'testing.xlsx', 'Predicted_Phases', predicted_df)
+        write_results(predicted_values, g_truth, index, f'{test_file_name}_result.xlsx')
     else:
         print(f"The file '{model_path}' does not exist.")
 
--- lstm.py
+++ lstm.py
@@ -9,15 +9,22 @@ from openpyxl.workbook import Workbook
 from tensorflow.keras.layers import Dense, LSTM, Concatenate, Input, Attention, Bidirectional
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 phase_list = ['A', 'B', 'C']
+num_columns = 0
+value_mapping = {0: 'A', 1: 'B', 2: 'C'}
 
 
 def load_data(file_path):
     if not os.path.exists(file_path):
         raise FileNotFoundError(file_path)
 
-    columns = [f"t{i}" for i in range(1, 1441)]
+    full_sheet = pd.read_excel(file_path, sheet_name='Full')
+    global num_columns
+    num_columns = len(full_sheet.columns) - 1
+
+    columns = [f"t{i}" for i in range(1, num_columns + 1)]
     data_full_train = pd.read_excel(file_path, sheet_name='Full', index_col=0, names=columns)
     g_truth_data = pd.read_excel(file_path, sheet_name='G truth')
 
@@ -31,11 +38,11 @@ def load_data(file_path):
     x = np.expand_dims(data_full_train.drop(columns=['PHASE']).to_numpy(), axis=(2,))
     y = to_categorical(data_full_train['PHASE'].to_numpy(), num_classes=3)
 
-    return x, y, data_full_train.index
+    return x, y, data_full_train.index, data_full_train['PHASE']
 
 
 def build_rnn_model():
-    _input = Input(shape=(1440, 1))
+    _input = Input(shape=(num_columns, 1))
 
     lstm_out = Bidirectional(LSTM(128))(_input)
     #lstm_out = Dropout(0.4)(lstm_out)
@@ -72,6 +79,7 @@ def add_or_update_excel_sheet(file_path, sheet_name, data):
         workbook = load_workbook(file_path)
     except FileNotFoundError:
         workbook = Workbook()
+        workbook.save(file_path)
 
     if sheet_name in workbook.sheetnames:
         workbook.remove(workbook[sheet_name])
@@ -81,29 +89,37 @@ def add_or_update_excel_sheet(file_path, sheet_name, data):
         data.to_excel(writer, sheet_name=sheet_name)
 
 
-def get_phase_labels(predicted_values):
-    rounded_values = np.round(predicted_values).astype(int)
+def get_phase_labels(predicted):
+    rounded_values = np.round(predicted).astype(int)
     phase_names = np.array(phase_list)
     phase_indices = np.argmax(rounded_values, axis=1)
     phase_labels = phase_names[phase_indices]
     return phase_labels
 
 
+def write_results(predicted, expected, idx, file_path):
+    phase_labels = get_phase_labels(predicted)
+    predicted_df = pd.DataFrame(phase_labels, index=idx, columns=['Predicted_PHASE'])
+
+    expected = expected.replace(value_mapping).to_frame()
+    merged_df = pd.concat([expected, predicted_df], axis=1)
+
+    add_or_update_excel_sheet(file_path, 'Predicted_Phases', merged_df)
+
+
 if __name__ == '__main__':
     remove_logs('logs')
-    x_train, y_train, index = load_data('training.xlsx')
-    print(x_train.shape)
-    print(y_train.shape)
+    train_file_name = input('Please enter excel file name that includes train data:(training?)').strip() or 'training'
+    x_train, y_train, index, g_truth = load_data(f'{train_file_name}.xlsx')
     model = build_rnn_model()
 
     tensorboard_callback = K.callbacks.TensorBoard(log_dir="./logs")
-    model.fit(x_train, y_train, epochs=256, batch_size=16, callbacks=[tensorboard_callback])
+    model_checkpoint_callback = ModelCheckpoint(filepath='model_checkpoint.keras')
+    model.fit(x_train, y_train, epochs=256, batch_size=16, callbacks=[tensorboard_callback, model_checkpoint_callback])
 
     # If it's fit to your consideration, save model
     save_model = input("Save current model?")
     if save_model == "y" or save_model == "Y":
         model.save('lstm_model.keras')
         predicted_values = model.predict(x_train)
-        phase_labels = get_phase_labels(predicted_values)
-        predicted_df = pd.DataFrame(phase_labels, index=index, columns=['Predicted_PHASE'])
-        add_or_update_excel_sheet(f'training.xlsx', 'Predicted_Phases', predicted_df)
+        write_results(predicted_values, g_truth, index, f'{train_file_name}_result.xlsx')
